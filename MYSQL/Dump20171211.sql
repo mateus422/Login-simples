d699c691b4c70c9c522232892d81cd5f197567ac
Mateus4222 <mateus13.aa@hotmail.com>
2017-12-11 03:24:35 -0300

    V.1.0 Reorganização
    -Arquivos desnecessários eliminados
    -MYSQL V.1.0 adicionado

---
+++ MYSQL/Dump20171211.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE  IF NOT EXISTS `cadastro` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `cadastro`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: cadastro
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.26-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(40) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `profissao` varchar(40) NOT NULL,
+  PRIMARY KEY (`codigo`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'mateus','mateus13.aa@hotmail.com','Estudante'),(2,'Mauricio','mau12341@outlook.com.br','Professor'),(3,'Bang','bang@gmail.com','Vendedor'),(4,'Eita Buh','eitabuh@gmail.com','Vendedor'),(5,'Cê tá','ceta@gmail.com','Professor '),(6,'Bonita','bonita@gmail.com','Estudante'),(34,'mateus','mateus14.aa@hotmail.com','Estudante'),(35,'mateus','mateus15.aa@hotmail.com','Estudante'),(36,'mateus','mau1231@outlook.com.br','professor'),(38,'mateus','mau123411@outlook.com.br','estudante'),(39,'mateus','mau1234211@outlook.com.br','estudante'),(40,'mateus','mau12324211@outlook.com.br','estudante'),(41,'mateus','33333333333@outlook.com.br','estudante'),(42,'mateus','333333333333@outlook.com.br','estudante'),(43,'mateus','3333333333333@outlook.com.br','estudante'),(44,'mateus','33333333333333@outlook.com.br','estudante'),(45,'mateus','mau1aaa2341@outlook.com.br','estudante'),(46,'mateus','mau123ws41@outlook.com.br','professor'),(47,'mateus','eitabqquh@gmail.com','Professor');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-12-11  3:23:02
